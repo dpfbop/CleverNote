05f31e74b338607a7224ce5773d9c419cc0bf887
Eugene Yurtaev <eugene-2306@ya.ru>
2015-06-22 11:27:24 -0700

    Initial Commit

---
+++ CleverNote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleverNote
+//
+//  Created by Eugene Yurtaev on 22/06/15.
+//  Copyright (c) 2015 dpfbop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
